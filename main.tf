1c1ee285776deec0649990257a5811cf8dcc078d
Artashes Arabajyan <araraba@thoughtworks.com>
2020-05-24 12:38:19 +0200

    create s3 bucket

--- main.tf
+++ main.tf
@@ -5,3 +5,13 @@ terraform {
 output "test" {
   value = "TEST"
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-q2eq2eq3eq3eqwe"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
